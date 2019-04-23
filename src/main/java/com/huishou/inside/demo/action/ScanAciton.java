bfed2372ca2197580274402c1bf5985394000206
1933839252 <1933839252@qq.com>
2019-04-23 16:28:18 +0800

    增加扫一扫接口，打包成jar包，端口换为8011

---
+++ src/main/java/com/huishou/inside/demo/action/ScanAciton.java
@@ -0,0 +1,41 @@
+package com.huishou.inside.demo.action;
+
+import net.sf.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+import java.net.URI;
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+
+public class ScanAciton {
+
+    @PostMapping("/api/admin/minip/{id}/ProviderUserlogin")
+    public String  ProviderUserlogin(@PathVariable String id){
+
+        RestTemplate restTemplate=new RestTemplate();
+        String url="http://dbsprod-console.dabashounb.com/api/admin/app/"+id+"/ProviderUserlogin";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("outId",38);
+        jsonObject.put("username","13800000000");
+        jsonObject.put("realname","minip");
+        jsonObject.put("type","repair");
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpEntity httpEntity = new HttpEntity(jsonObject.toString(), header);
+        String response = restTemplate.postForObject(url, httpEntity, String.class);
+        return response.toString();
+    }
+}
